96dc258372c3af7d3c49f8ffaaae0cb451d57ff8
yr5698 <yr5698@gmail.com>
2021-12-05 16:57:31 +0900

    pc client111

---
+++ ChatProject/src/PcClient.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+import java.net.Socket;
+
+public class PcClient {
+	
+	public static void main(String[] args)
+	{
+		try {
+
+			Socket c_socket = new Socket("192.168.0.23", 8888);
+			
+			ReceiveT rec_thread = new ReceiveT();
+			rec_thread.setSocket(c_socket);
+			
+			SendT send_thread = new SendT();
+			send_thread.setSocket(c_socket);
+			
+			send_thread.start();
+			rec_thread.start();
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
